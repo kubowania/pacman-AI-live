304fefd871168076140791778609311392830de9
Ania Kubow <kubowania@gmail.com>
2020-10-31 11:06:05 +0000

    url loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,17 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      },
       { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
       { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] }
     ]
